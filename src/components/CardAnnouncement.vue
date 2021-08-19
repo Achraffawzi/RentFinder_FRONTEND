b680cd663341608c8663506c153788c330a8e61f
achraffawzi <achraffawzi224a@gmail.com>
2021-08-19 19:53:41 +0200

    Adding announcement card

---
+++ src/components/CardAnnouncement.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card-announcement">
+    <v-container>
+      <div class="d-flex">
+        <img src="https://source.unsplash.com/random/60x60" alt="" />
+        <v-card outlined
+          >
+          <v-card-title class="d-flex align-center justify-space-between">
+            <h5 class="text-h5 font-weight-bold">$55/night</h5>
+            <v-icon>favorite_border</v-icon>
+          </v-card-title>
+          <v-card-text>
+            <!-- content -->
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            <v-rating length="5" value="4" color="yellow"></v-rating>
+          </v-card-text>
+          <v-card-actions>
+            <span class="font-weight-medium mr-4">Agadir</span>
+            <span class="font-weight-medium">23/05/2000</span>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardAnnouncement",
+  props: ["housesResult"]
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,27 @@
     <div class="home">
         <Banner />
         <GlobalStatistics />
+
+        <!-- Featured Houses -->
+        <section class="featured-houses py-16">
+            <v-container>
+                <h4 class="text-h4 font-weight-bold mb-4">Featured Houses</h4>
+            </v-container>
+            <CardAnnouncement />
+        </section>
     </div>
 </template>
 
 <script>
 import Banner from "@/components/home/Banner.vue";
 import GlobalStatistics from "@/components/GlobalStatistics.vue";
+import CardAnnouncement from "@/components/CardAnnouncement.vue";
 export default {
     name: "Home",
     components: {
         Banner,
         GlobalStatistics,
+        CardAnnouncement,
     },
 }
 </script>
