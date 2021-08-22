78a9c2f4e256c647d0760ce830ca2e007e790161
achraffawzi <achraffawzi224a@gmail.com>
2021-08-22 13:46:02 +0200

    Adding data table to the house owner dashboard

--- src/views/houseOwner/HouseOwnerDashboard.vue
+++ src/views/houseOwner/HouseOwnerDashboard.vue
@@ -13,10 +13,117 @@
 
       <!-- Statistic numbers -->
       <v-row>
-          <v-col cols="12" sm="4" v-for="item in houseNumbers" :key="item.title">
-              <DashboardNumbers  :data="item" />
-          </v-col>
+        <v-col cols="12" sm="4" v-for="item in houseNumbers" :key="item.title">
+          <DashboardNumbers :data="item" />
+        </v-col>
       </v-row>
+
+      <!-- Data Table -->
+      <v-data-table
+        :headers="headers"
+        :items="announcements"
+        sort-by="calories"
+        class="elevation-1 mt-16"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>
+              <v-btn class="text-lowercase">Sort by price</v-btn>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  class="mb-2"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  New Item
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">{{ formTitle }}</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.name"
+                          label="Dessert name"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.calories"
+                          label="Calories"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.fat"
+                          label="Fat (g)"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.carbs"
+                          label="Carbs (g)"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field
+                          v-model="editedItem.protein"
+                          label="Protein (g)"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">
+                    Cancel
+                  </v-btn>
+                  <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+              <v-card>
+                <v-card-title class="text-h5"
+                  >Are you sure you want to delete this item?</v-card-title
+                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="closeDelete"
+                    >Cancel</v-btn
+                  >
+                  <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                    >OK</v-btn
+                  >
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize"> Reset </v-btn>
+        </template>
+      </v-data-table>
     </v-container>
   </div>
 </template>
@@ -30,6 +137,7 @@ export default {
   },
   data() {
     return {
+      // Statistics Numbers
       houseNumbers: [
         {
           title: "Available Houses",
@@ -47,7 +155,146 @@ export default {
           value: 16,
         },
       ],
+      dialog: false,
+      dialogDelete: false,
+
+      // Data table properties
+      headers: [
+        {
+          text: "Title",
+          align: "start",
+          value: "title",
+        },
+
+        { text: "Description", value: "description" },
+        { text: "Price", value: "price" },
+        { text: "City", value: "city" },
+        { text: "Total Floors", value: "totalfloors" },
+        { text: "Total Bathrooms", value: "totalbathrooms" },
+        { text: "Total Livingrooms", value: "totallivingrooms" },
+        { text: "Total Kitchens", value: "totalkitchens" },
+        { text: "Total Bedrooms", value: "totalbedrooms" },
+        { text: "Surface", value: "surface" },
+        { text: "Available", value: "isAvailable" },
+        { text: "Photos", value: "photos" },
+        { text: "Actions", value: "actions", sortable: false },
+      ],
+      announcements: [],
+      editedIndex: -1,
+      editedItem: {
+        id: 0,
+        title: "",
+        photos: "",
+        city: "",
+        price: 0,
+        description: "",
+        totalfloors: 0,
+        totalbathrooms: 0,
+        totallivingrooms: 0,
+        totalkitchens: 0,
+        totalbedrooms: 0,
+        surface: 0,
+        isAvailable: true,
+      },
+      defaultItem: {
+        id: 0,
+        title: "",
+        photos: "",
+        city: "",
+        price: 0,
+        description: "",
+        totalfloors: 0,
+        totalbathrooms: 0,
+        totallivingrooms: 0,
+        totalkitchens: 0,
+        totalbedrooms: 0,
+        surface: 0,
+        isAvailable: true,
+      },
     };
+  },
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.announcements = [
+        {
+          id: 12,
+          title: "qsd",
+          photos: "qsd",
+          city: "qsd",
+          price: "qsd",
+          description: "qsd",
+          totalfloors: "qsd",
+          totalbathrooms: "qsd",
+          totallivingrooms: "qsd",
+          totalkitchens: "qsd",
+          totalbedrooms: "qsd",
+          surface: "qsd",
+          isAvailable: "qsd",
+        },
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.announcements.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.announcements.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      this.announcements.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.announcements[this.editedIndex], this.editedItem);
+      } else {
+        this.announcements.push(this.editedItem);
+      }
+      this.close();
+    },
   },
 };
 </script>
