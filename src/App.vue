b41b4f2aad8bd5da7a620c73dca85cf038a8f51b
achraffawzi <achraffawzi224a@gmail.com>
2021-09-11 13:23:10 +0200

    Customizing scrollbar

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,28 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+html {
+  scrollbar-width: thin;
+}
+
+html::-webkit-scrollbar {
+  width: 1vw;
+}
+ 
+html::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+}
+ 
+html::-webkit-scrollbar-thumb {
+  background-color: rgba(0, 141, 233, .7);
+  outline: 1px solid slategrey;
+}
+
+html::-webkit-scrollbar-thumb:hover {
+  background-color: $primary-color;
+}
+
 #app {
   display: flex;
   flex-direction: column;
